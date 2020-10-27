304cadf2d5773b61e3568475dbd2bfff578df02b
Eyes0n <eye3570@naver.com>
2020-10-27 18:45:34 +0900

    apply Posts dummy data to mongoDB

--- src/main.js
+++ src/main.js
@@ -4,18 +4,20 @@ import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
 
+import api from './api';
+import createFakeData from './createFakeData';
+
 const { PORT, MONGO_URL } = process.env;
 
 mongoose
   .connect(MONGO_URL, { useNewUrlParser: true, useFindAndModify: false })
   .then(() => {
     console.log('Conneted to MongoDB');
+    createFakeData(); // dummy data
   })
   .catch((e) => {
     console.error(e);
   });
-
-import api from './api';
 
 const app = new Koa();
 const router = new Router();
