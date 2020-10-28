5afb0173982e300e95e745ac493cfe92e01e2ce1
Eyes0n <eye3570@naver.com>
2020-10-28 22:28:11 +0900

    set decoded(jwt info) to be used in Middleware

--- src/lib/jwtMiddleware.js
+++ src/lib/jwtMiddleware.js
@@ -2,11 +2,16 @@ import jwt from 'jsonwebtoken';
 
 const jwtMiddleware = (ctx, next) => {
   const token = ctx.cookies.get('access_token');
-  if (!token) return next();
+  if (!token) return next(); // 토큰이 없음
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log(decoded);
+    // decoded를 미들웨어에서 사용할려면 ctx.state에 넣어준다
+    ctx.state.user = {
+      id: decoded.id,
+      username: decoded.username,
+    };
+    console.log('decoded JWT:', decoded);
     return next();
   } catch (error) {
     // 토큰 검증 실패
