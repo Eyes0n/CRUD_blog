3735e41b3882f7e1ff86fa93881ec66b0b928a37
Eyes0n <eye3570@naver.com>
2020-10-28 22:30:22 +0900

    add auth check(login state) func

--- src/api/auth/auth.ctrl.js
+++ src/api/auth/auth.ctrl.js
@@ -92,8 +92,17 @@ export const login = async (ctx) => {
   }
 };
 
+/*
+  GET /api/auth/check
+*/
 export const check = async (ctx) => {
-  // 로그인 상태 확인
+  const { user } = ctx.state;
+  if (!user) {
+    // 로그인 중 아님(토큰 미발행)
+    ctx.status = 401; // Unauthorized
+    return;
+  }
+  ctx.body = user;
 };
 
 export const logout = async (ctx) => {
