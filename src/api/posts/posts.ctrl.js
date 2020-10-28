5196fdd973094eacc1fb89cf6ffd4efa161a037c
Eyes0n <eye3570@naver.com>
2020-10-29 01:23:18 +0900

    add userInfo when post writing

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -38,7 +38,12 @@ export const write = async (ctx) => {
   }
 
   const { title, body, tags } = ctx.request.body;
-  const post = new Post({ title, body, tags });
+  const post = new Post({
+    title,
+    body,
+    tags,
+    user: ctx.state.user,
+  });
   try {
     await post.save();
     ctx.body = post;
