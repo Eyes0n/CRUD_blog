02b18d5b61ae6c68412192e95f57b8cbb14451b9
Eyes0n <eye3570@naver.com>
2020-10-28 16:16:11 +0900

    modify annotation(주석)

--- src/models/user.js
+++ src/models/user.js
@@ -6,21 +6,25 @@ const UserSchema = new Schema({
   hashedPassword: String,
 });
 
-// model instance method for password
-// user.setPassword()
+/*  
+Model Method
+  1.Instance of Document Method 
+    user.setPassword() checkpassword()
+  2.Static Method(Model Instance=Document)
+    findByUsername()
+*/
 UserSchema.methods.setPassword = async function (password) {
   const hash = await bcrypt.hash(password, 10);
   this.hashedPassword = hash; // this -> document instance
 };
-// user.checkpassword()
+
 UserSchema.methods.checkPassword = async function (password) {
   const result = await bcrypt.compare(password, this.hashedPassword);
   return result; // true||false
 };
 
-// static method
 UserSchema.statics.findByUsername = async function (username) {
-  return this.findOne({ username }); // this -> User(model instance)
+  return this.findOne({ username }); // this -> User(model instance = doc)
 };
 
 const User = mongoose.model('User', UserSchema);
