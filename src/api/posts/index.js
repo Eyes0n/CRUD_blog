35631cf4e031c95d5f478434797bd8bc0f2a8afd
Eyes0n <eye3570@naver.com>
2020-10-29 01:03:50 +0900

    apply checkLoggedIn middleware

--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -1,15 +1,16 @@
 import Router from 'koa-router';
+import checkLoggedIn from '../../lib/checkLoggeIn';
 import * as postsCtrl from './posts.ctrl';
 
 const posts = new Router();
 
 posts.get('/', postsCtrl.list);
-posts.post('/', postsCtrl.write);
+posts.post('/', checkLoggedIn, postsCtrl.write);
 
 const post = new Router(); // /api/posts/:id
 post.get('/', postsCtrl.list);
-post.delete('/', postsCtrl.remove);
-post.patch('/', postsCtrl.update);
+post.delete('/', checkLoggedIn, postsCtrl.remove);
+post.patch('/', checkLoggedIn, postsCtrl.update);
 
 posts.use('/:id', postsCtrl.checkObjectId, post.routes());
 
