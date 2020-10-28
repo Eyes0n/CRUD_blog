7979e43947a2a929cfaa6801d5781d39eeef8866
Eyes0n <eye3570@naver.com>
2020-10-28 17:02:06 +0900

    add auth register func & apply serialize method

--- src/api/auth/auth.ctrl.js
+++ src/api/auth/auth.ctrl.js
@@ -1,12 +1,57 @@
+import Joi from 'joi';
+import User from '../../models/user';
+
+/*
+  POST /api/auth/register
+  {
+    username: 'velopert',
+    password: 'mypass123'
+  }
+*/
 export const register = async (ctx) => {
-  // 회원가입
+  // Requset Body 검증하기
+  const schema = Joi.object().keys({
+    username: Joi.string().alphanum().min(3).max(20).required(),
+    password: Joi.string().required(),
+  });
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+
+  const { username, password } = ctx.request.body;
+  try {
+    // username이 이미 존재하는지 확인
+    const exists = await User.findByUsername(username);
+    if (exists) {
+      ctx.status = 409; // Conflict
+      return;
+    }
+
+    const user = new User({
+      username,
+    });
+    await user.setPassword(password); // password 설정
+    await user.save(); // DB에 저장
+
+    // 응답할 데이터에서 hashedPassword 제거
+    const data = user.serialize();
+    ctx.body = data;
+  } catch (error) {
+    ctx.throw(500, error);
+  }
 };
+
 export const login = async (ctx) => {
   // 로그인
 };
+
 export const check = async (ctx) => {
   // 로그인 상태 확인
 };
+
 export const logout = async (ctx) => {
   // 로그아웃
 };
