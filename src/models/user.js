a8ba37f6c570679203515382d60e632ca798018b
Eyes0n <eye3570@naver.com>
2020-10-28 14:30:03 +0900

    create model static method to find user

--- src/models/user.js
+++ src/models/user.js
@@ -10,13 +10,19 @@ const UserSchema = new Schema({
 // user.setPassword()
 UserSchema.methods.setPassword = async function (password) {
   const hash = await bcrypt.hash(password, 10);
-  this.hashedPassword = hash;
+  this.hashedPassword = hash; // this -> document instance
 };
 // user.checkpassword()
 UserSchema.methods.checkPassword = async function (password) {
   const result = await bcrypt.compare(password, this.hashedPassword);
   return result; // true||false
 };
+
+// static method
+UserSchema.statics.findByUsername = async function (username) {
+  return this.findOne({ username }); // this -> User(model instance)
+};
+
 const User = mongoose.model('User', UserSchema);
 
 export default User;
