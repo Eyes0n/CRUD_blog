85b8a45bd09e993595ce97e4d94b5b670d505750
Eyes0n <eye3570@naver.com>
2020-10-28 22:26:25 +0900

    apply jwtMiddleware

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
 
 import api from './api';
+import jwtMiddleware from './lib/jwtMiddleware';
 
 const { PORT, MONGO_URL } = process.env;
 
@@ -25,6 +26,9 @@ router.use('/api', api.routes()); // api 라우트 적용
 
 // 라우터 적용 전에 bodyParser 적용
 app.use(bodyParser());
+
+// 라우터 적용 전에 jwt verfication Middleware 적용
+app.use(jwtMiddleware);
 
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
